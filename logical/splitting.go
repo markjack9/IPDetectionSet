20aeb0e539665bbc1d10b2e2d75fbf04ab35a618
mark.yan <m15901368529@gmail.com>
2024-03-26 11:32:51 +0800

    修改报警格式

--- logical/splitting.go
+++ logical/splitting.go
@@ -9,18 +9,19 @@ import (
 	"strings"
 )
 
-func PingJudgment(url string, token string, starttime string, devicetype string) {
-	var successDevices []mode.DeviceCheckInfo
-	var failedDevices []mode.DeviceCheckInfo
-	failedfilename := devicetype + bin.GetCurrentTimeName() + ".csv"
+func PingJudgment(url string) (failedDevices []mode.DeviceCheckInfo, IPs []string) {
+	//var successDevices []mode.DeviceCheckInfo
+	//var failedDevices []mode.DeviceCheckInfo
+
+	//failedfilename := devicetype + bin.GetCurrentTimeName() + ".csv"
 	// 用于存储循环中的信息的字符串
-	var content, failedContent strings.Builder
+	//var content, failedContent strings.Builder
 	deviceInfo := db.PullData(url)
 
 	for _, info := range deviceInfo {
 		CheackResult := bin.PingCheack(info.IPInner, 3)
 		var status string
-		if CheackResult == false {
+		if CheackResult != true {
 			status = "Failed"
 			// 添加到失败的设备切片中
 			failedDevices = append(failedDevices, mode.DeviceCheckInfo{
@@ -29,64 +30,96 @@ func PingJudgment(url string, token string, starttime string, devicetype string)
 				Note:   info.AssMemo,
 				Status: status,
 			})
-		} else {
-			status = "Success"
-			// 添加到成功的设备切片中
-			successDevices = append(successDevices, mode.DeviceCheckInfo{
-				Ip:     info.IPInner,
-				Name:   info.Name,
-				Note:   info.AssMemo,
-				Status: status,
-			})
+			IPs = append(IPs, info.IPInner)
+			//} else {
+			//	status = "Success"
+			//	// 添加到成功的设备切片中
+			//	successDevices = append(successDevices, mode.DeviceCheckInfo{
+			//		Ip:     info.IPInner,
+			//		Name:   info.Name,
+			//		Note:   info.AssMemo,
+			//		Status: status,
+			//	})
 		}
 
 	}
-	// 打印成功的设备信息
-	content.WriteString("检测开始时间： " + starttime + "\n检测设备类型：" + devicetype + "\n检测结束时间： " + bin.GetCurrentTime() + "\n \n状态正常设备:\n")
-	for _, device := range successDevices {
-		SuccessInfo := fmt.Sprintf("类型: %s, IP: %s, 备注: %s\n", device.Name, device.Ip, device.Note)
-		//SuccessInfo := fmt.Sprintf("|类型|IP|备注| \n|-------|-----|-----|\n|%s|%s|%s|\n", device.Name, device.Ip, device.Note)
-		content.WriteString(SuccessInfo)
-	}
-
-	failedContent.WriteString("检测开始时间： " + starttime + "\n检测设备类型：" + devicetype + "\n检测结束时间： " + bin.GetCurrentTime() + "\n状态异常设备:\n")
-	// 打印失败的设备信息
-	for _, device := range failedDevices {
-		FailedInfo := fmt.Sprintf("类型: %s, IP: %s, 备注: %s\n", device.Name, device.Ip, device.Note)
-		//FailedInfo := fmt.Sprintf("|类型|IP|备注| \n|-------|-----|-----|\n|%s|%s|%s|\n", device.Name, device.Ip, device.Note)
-		failedContent.WriteString(FailedInfo)
-	}
+	//// 打印成功的设备信息
+	//content.WriteString("检测开始时间： " + starttime + "\n检测设备类型：" + devicetype + "\n检测结束时间： " + bin.GetCurrentTime() + "\n \n状态正常设备:\n")
+	//for _, device := range successDevices {
+	//	SuccessInfo := fmt.Sprintf("类型: %s, IP: %s, 备注: %s\n", device.Name, device.Ip, device.Note)
+	//	//SuccessInfo := fmt.Sprintf("|类型|IP|备注| \n|-------|-----|-----|\n|%s|%s|%s|\n", device.Name, device.Ip, device.Note)
+	//	content.WriteString(SuccessInfo)
+	//}
+	//
+	//failedContent.WriteString("检测开始时间： " + starttime + "\n检测设备类型：" + devicetype + "\n检测结束时间： " + bin.GetCurrentTime() + "\n状态异常设备:\n")
+	//// 打印失败的设备信息
+	//for _, device := range failedDevices {
+	//	FailedInfo := fmt.Sprintf("类型: %s, IP: %s\n", device.Name, device.Ip)
+	//	//FailedInfo := fmt.Sprintf("|类型|IP|备注| \n|-------|-----|-----|\n|%s|%s|%s|\n", device.Name, device.Ip, device.Note)
+	//	failedContent.WriteString(FailedInfo)
+	//}
+	return
 	//// 调用函数将成功和失败设备信息写入 CSV 文件
 	//err := bin.WriteDeviceDataToCSV(successDevices, "tmp/sucees"+devicetype+bin.GetCurrentTimeName()+".csv")
 	//if err != nil {
 	//	log.Println("Failed to write success devices to CSV:", err)
 	//}
-	if failedDevices != nil {
-		err := bin.WriteDeviceDataToCSV(failedDevices, "tmp/"+failedfilename)
-		if err != nil {
-			log.Println("Failed to write failed devices to CSV:", err)
-		}
-		media, err := bin.UploadMedia(token, failedfilename)
-		if err != nil {
-			return
-		}
+	//if failedDevices != nil {
+	//err := bin.WriteDeviceDataToCSV(failedDevices, "tmp/"+failedfilename)
+	//if err != nil {
+	//	log.Println("Failed to write failed devices to CSV:", err)
+	//}
+	//media, err := bin.UploadMedia(token, failedfilename)
+	//if err != nil {
+	//	return
+	//}
+	//
+	//err = bin.SendMessage(token, failedContent.String(), 4096)
+	////err = bin.SendMessage(token, content.String()+failedContent.String(), 4096)
+	//if err != nil {
+	//	return
+	//}
+	//err = bin.SendFile(token, media)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//} else {
 
-		err = bin.SendMessage(token, failedContent.String(), 4096)
-		//err = bin.SendMessage(token, content.String()+failedContent.String(), 4096)
-		if err != nil {
-			return
-		}
-		err = bin.SendFile(token, media)
-		if err != nil {
-			fmt.Println(err)
-		}
-	} else {
+	//err := bin.SendMessage(token, "检测开始时间： "+starttime+"\n检测设备类型："+devicetype+"\n检测结束时间： "+bin.GetCurrentTime()+"\n设备检测无异常\n", 4096)
+	////err = bin.SendMessage(token, content.String()+failedContent.String(), 4096)
+	//if err != nil {
+	//	return
+	//}
+	//}
 
-		err := bin.SendMessage(token, "检测开始时间： "+starttime+"\n检测设备类型："+devicetype+"\n检测结束时间： "+bin.GetCurrentTime()+"\n设备检测无异常\n", 4096)
-		//err = bin.SendMessage(token, content.String()+failedContent.String(), 4096)
-		if err != nil {
-			return
-		}
+}
+
+func Infomerging(starttime string, token string, faileinfo []mode.DeviceCheckInfo, longtxet string) error {
+	failedfilename := bin.GetCurrentTimeName() + ".csv"
+	var failedContent strings.Builder
+	failedContent.WriteString("IT\n" + "检测开始时间： " + starttime + "\n检测结束时间： " + bin.GetCurrentTime() + "\n" + "\n状态异常设备:\n" + longtxet)
+	//for _, device := range faileinfo {
+	//	FailedInfo := fmt.Sprintf("类型: %s, IP: %s\n", device.Name, device.Ip)
+	//	//FailedInfo := fmt.Sprintf("|类型|IP|备注| \n|-------|-----|-----|\n|%s|%s|%s|\n", device.Name, device.Ip, device.Note)
+	//	failedContent.WriteString(FailedInfo)
+	//}
+	err := bin.WriteDeviceDataToCSV(faileinfo, "tmp/"+failedfilename)
+	if err != nil {
+		log.Println("Failed to write failed devices to CSV:", err)
+	}
+	media, err := bin.UploadMedia(token, failedfilename)
+	if err != nil {
+		return err
 	}
 
+	err = bin.SendMessage(token, failedContent.String(), 4096)
+	//err = bin.SendMessage(token, content.String()+failedContent.String(), 4096)
+	if err != nil {
+		return err
+	}
+	err = bin.SendFile(token, media)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return err
 }
--- main.go
+++ main.go
@@ -3,25 +3,65 @@ package main
 import (
 	"IP_Detection_Set/bin"
 	"IP_Detection_Set/logical"
+	"IP_Detection_Set/mode"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
+	var DeviceMerging []mode.DeviceCheckInfo
+	var IpMerging []string
+	var failedContentMerging strings.Builder
+	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	starttime := bin.GetCurrentTime()
+	NewDeviceInfo, ipsinfo := checkPrinter()
+	DeviceMerging = append(DeviceMerging, NewDeviceInfo...)
+	IpMerging = append(IpMerging, ipsinfo...)
+	if NewDeviceInfo != nil {
+		failedContentMerging.WriteString("\n打印机:\n" + strings.Join(IpMerging, ","))
+	}
+	IpMerging = nil
+	time.Sleep(3 * time.Second)
+	NewDeviceInfo, ipsinfo = checkAP()
+	DeviceMerging = append(DeviceMerging, NewDeviceInfo...)
+	IpMerging = append(IpMerging, ipsinfo...)
+	if NewDeviceInfo != nil {
+		failedContentMerging.WriteString("\nAP:\n" + strings.Join(IpMerging, ","))
+	}
 
-	checkPrinter()
+	IpMerging = nil
 	time.Sleep(3 * time.Second)
-	checkAP()
+	NewDeviceInfo, ipsinfo = checkBroadcast()
+	DeviceMerging = append(DeviceMerging, NewDeviceInfo...)
+	IpMerging = append(IpMerging, ipsinfo...)
+	if NewDeviceInfo != nil {
+		failedContentMerging.WriteString("\n广播:\n" + strings.Join(IpMerging, ","))
+	}
+	IpMerging = nil
 	time.Sleep(3 * time.Second)
-	checkBroadcast()
+	NewDeviceInfo, ipsinfo = checkCamera()
+	DeviceMerging = append(DeviceMerging, NewDeviceInfo...)
+	IpMerging = append(IpMerging, ipsinfo...)
+	if NewDeviceInfo != nil {
+		failedContentMerging.WriteString("\n摄像头:\n" + strings.Join(IpMerging, ","))
+	}
+	IpMerging = nil
 	time.Sleep(3 * time.Second)
-	checkCamera()
-	time.Sleep(3 * time.Second)
-	checkAccessControl()
+	NewDeviceInfo, ipsinfo = checkAccessControl()
 	fmt.Println("所有任务已完成")
-
+	DeviceMerging = append(DeviceMerging, NewDeviceInfo...)
+	IpMerging = append(IpMerging, ipsinfo...)
+	if NewDeviceInfo != nil {
+		failedContentMerging.WriteString("\n门禁:\n" + strings.Join(IpMerging, ","))
+	}
+	IpMerging = nil
+	err := logical.Infomerging(starttime, qyapitoken, DeviceMerging, failedContentMerging.String())
+	if err != nil {
+		log.Println(err)
+	}
 	// 创建日志文件
 	file, err := os.OpenFile("log/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -46,56 +86,61 @@ func main() {
 }
 
 // 检测打印机任务
-func checkPrinter() {
+func checkPrinter() (infomerging []mode.DeviceCheckInfo, ips []string) {
 	fmt.Println("正在检测打印机...")
 	url := "http://simba.gyyx.cn/specific_assets?atype=32"
-	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
-	currentTime := bin.GetCurrentTime()
-	devicetype := "打印机"
-	logical.PingJudgment(url, qyapitoken, currentTime, devicetype)
+	//qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	//currentTime := bin.GetCurrentTime()
+	//devicetype := "打印机"
+	infomerging, ips = logical.PingJudgment(url)
+	return
 	// 这里放置打印机检测的具体逻辑
 }
 
 // 检测AP任务
-func checkAP() {
+func checkAP() (infomerging []mode.DeviceCheckInfo, ips []string) {
 	fmt.Println("正在检测AP...")
 	url := "http://simba.gyyx.cn/specific_assets?atype=47"
-	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
-	currentTime := bin.GetCurrentTime()
-	devicetype := "AP"
-	logical.PingJudgment(url, qyapitoken, currentTime, devicetype)
+	//qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	//currentTime := bin.GetCurrentTime()
+	//devicetype := "AP"
+	infomerging, ips = logical.PingJudgment(url)
+	return
 	// 这里放置AP检测的具体逻辑
 }
 
 // 检测广播任务
-func checkBroadcast() {
+func checkBroadcast() (infomerging []mode.DeviceCheckInfo, ips []string) {
 	fmt.Println("正在检测广播...")
 	url := "http://simba.gyyx.cn/specific_assets?atype=50"
-	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
-	currentTime := bin.GetCurrentTime()
-	devicetype := "广播"
-	logical.PingJudgment(url, qyapitoken, currentTime, devicetype)
+	//qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	//currentTime := bin.GetCurrentTime()
+	//devicetype := "广播"
+	infomerging, ips = logical.PingJudgment(url)
+	return
 	// 这里放置广播检测的具体逻辑
 }
 
 // 检测摄像头任务
-func checkCamera() {
+func checkCamera() (infomerging []mode.DeviceCheckInfo, ips []string) {
 	fmt.Println("正在检测摄像头...")
 	url := "http://simba.gyyx.cn/specific_assets?atype=49"
-	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
-	currentTime := bin.GetCurrentTime()
-	devicetype := "摄像头"
-	logical.PingJudgment(url, qyapitoken, currentTime, devicetype)
+	//qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	//currentTime := bin.GetCurrentTime()
+	//devicetype := "摄像头"
+	infomerging, ips = logical.PingJudgment(url)
+	return
 	// 这里放置摄像头检测的具体逻辑
 }
 
 // 打印机检测任务
-func checkAccessControl() {
+func checkAccessControl() (infomerging []mode.DeviceCheckInfo, ips []string) {
 	fmt.Println("正在检测门禁...")
 	url := "http://simba.gyyx.cn/specific_assets?atype=51"
-	qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
-	currentTime := bin.GetCurrentTime()
-	devicetype := "门禁"
-	logical.PingJudgment(url, qyapitoken, currentTime, devicetype)
+	//qyapitoken := "c2ae10dc-c831-4d08-a573-f1515caedc6b"
+	//currentTime := bin.GetCurrentTime()
+	//devicetype := "门禁"
+	infomerging, ips = logical.PingJudgment(url)
+	return
 	// 模拟打印机检测过程
 }
